f1df247966c9b1422ef17fcc79763aa609facbad
Tanner Nielsen <tannerntannern@gmail.com>
2019-04-30 23:19:55 -0500

    test: finished testing the core api

--- src/index.ts
+++ src/index.ts
@@ -41,11 +41,13 @@ export type Adapter<R, O, I> = {
  */
 export const makeAdapter = <R = any, O = any, I = Record>(executor: AdapterExecutor<R, O, I>): Adapter<R, O, I> => {
 	// Default then, catch, output, and input attachments
-	let then: Resolve<R> = (result) => result;
-	let cach: Reject = (err) => {throw err;};
-	let output: Output<O> = () => {};
-	let input: Input<I> = async (key) => {
-		throw new Error(`Input "${key}" must be supplied`);
+	const attachments: {then: Resolve<R>, cach: Reject, output: Output<O>, input: Input<I>} = {
+		then: (result) => result,
+		cach: (err) => {throw err;},
+		output: () => {},
+		input: async (key) => {
+			throw new Error(`Input "${key}" must be supplied`);
+		}
 	};
 
 	// Builds a PromiseExecutor from the given AdapterExecutor.  Since our executor may or may not be async, we need
@@ -53,28 +55,25 @@ export const makeAdapter = <R = any, O = any, I = Record>(executor: AdapterExecu
 	const makePromiseExecutor = (): PromiseExecutor<R> =>
 		async (resolve: Resolve<R>, reject: Reject) => {
 			try {
-				await executor(resolve, reject, output, input);
+				await executor(resolve, reject, attachments.output, attachments.input);
 			} catch (e) {
 				reject(e);
 			}
 		};
 
 	// Makes a standard Promise from our current then, catch, output, and input functions
-	const makePromise = (): Promise<R> => new Promise<R>(makePromiseExecutor()).then(then).catch(cach) as unknown as Promise<R>;
+	const makePromise = (): Promise<R> =>
+		new Promise<R>(makePromiseExecutor())
+			.then(attachments.then)
+			.catch(attachments.cach) as unknown as Promise<R>;
 
 	return {
-		exec: () => makePromise(),
-		promise: () => makePromise(),
-		then: function (onThen) { then = onThen; return this; },
-		catch: function (onCatch) { cach = onCatch; return this; },
-		output: function (onStatus) { output = onStatus; return this; },
-		input: function (onInput) { input = onInput; return this; },
-		attach: function(adapters) {
-			if (adapters.then) this.then(adapters.then);
-			if (adapters.catch) this.catch(adapters.catch);
-			if (adapters.output) this.catch(adapters.output);
-			if (adapters.input) this.catch(adapters.input);
-			return this;
-		}
+		exec: makePromise,
+		promise: makePromise,
+		then: function(onThen) { attachments.then = onThen; return this; },
+		catch: function(onCatch) { attachments.cach = onCatch; return this; },
+		output: function(onStatus) { attachments.output = onStatus; return this; },
+		input: function(onInput) { attachments.input = onInput; return this; },
+		attach: function(adapters) { Object.assign(attachments, adapters); return this; }
 	};
 };
--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,94 +1,150 @@
 import 'mocha';
-import {expect} from 'chai';
+import {use, expect} from 'chai';
 import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+use(sinonChai);
 
 import {makeAdapter} from '../../src';
 
 describe('makeAdapter(...)', () => {
-	describe('as "normal" promise', () => {
-		const simplePromiseLikeFunction = (succeed: boolean) => makeAdapter<string>((resolve, reject) => {
-			if (succeed) resolve('result');
-			else reject('somethin aint right');
-		});
+	describe('basic usage', () => {
+		describe('as "normal" promise', () => {
+			const simplePromiseLikeFunction = (succeed: boolean) => makeAdapter<string>((resolve, reject) => {
+				if (succeed) resolve('result');
+				else reject('somethin aint right');
+			});
 
-		it('should resolve properly', async () => {
-			const then = sinon.fake();
+			it('should resolve properly', async () => {
+				const then = sinon.fake();
 
-			await (simplePromiseLikeFunction(true).promise().then(then));
+				await (simplePromiseLikeFunction(true).promise().then(then));
 
-			expect(then.calledOnceWithExactly('result')).to.be.true;
-		});
+				expect(then).calledOnceWithExactly('result');
+			});
 
-		it('should reject properly', async () => {
-			const catcher = sinon.fake();
+			it('should reject properly', async () => {
+				const catcher = sinon.fake();
 
-			await (simplePromiseLikeFunction(false).promise().catch(catcher));
+				await (simplePromiseLikeFunction(false).promise().catch(catcher));
 
-			expect(catcher.calledOnceWithExactly('somethin aint right')).to.be.true;
-		});
-	});
-
-	describe('with status reporting', () => {
-		const buildArray = () => makeAdapter<number[]>(async (resolve, reject, output) => {
-			let array = [];
-			for (let i = 0; i < 3; i ++) {
-				output({msg: 'Pushing data', value: i});
-				array.push(i);
-			}
-			resolve(array);
-		});
-
-		it('should resolve properly', async () => {
-			const then = sinon.fake();
-
-			await (buildArray().promise().then(then));
-
-			expect(then.calledOnceWithExactly([0, 1, 2])).to.be.true;
-		});
-
-		it('should properly report status', async () => {
-			const onOutput = sinon.fake();
-
-			await (buildArray().output(onOutput).promise());
-
-			expect(onOutput.callCount).to.equal(3);
-			expect(onOutput.calledWith({msg: 'Pushing data', value: 0})).to.be.true;
-			expect(onOutput.calledWith({msg: 'Pushing data', value: 1})).to.be.true;
-			expect(onOutput.calledWith({msg: 'Pushing data', value: 2})).to.be.true;
-		});
-	});
-
-	describe('with input requests', () => {
-		const postInputSpy = sinon.fake();
-		const getPassword = () => makeAdapter<number, string, any>(async (resolve, reject, output, input) => {
-			output('asking for password');
-			const password = await input('password');
-			postInputSpy();
-			output('got password');
-			const passwordLength = password.length;
-			resolve(passwordLength);
-		});
-
-		it('should throw an error if no .input() is used', () => {
-			getPassword().promise().catch(error => {
-				expect(postInputSpy.notCalled).to.be.true;
-				expect(error.message).to.equal('Input "password" must be supplied');
+				expect(catcher).calledOnceWithExactly('somethin aint right');
 			});
 		});
 
-		it('should receive the input properly', async () => {
-			const inputReceiver = sinon.fake.resolves('pa$$word');
-			const outputReceiver = sinon.fake();
+		describe('with .output()', () => {
+			const buildArray = () => makeAdapter<number[]>(async (resolve, reject, output) => {
+				let array = [];
+				for (let i = 0; i < 3; i ++) {
+					output({msg: 'Pushing data', value: i});
+					array.push(i);
+				}
+				resolve(array);
+			});
 
-			const passwordLength = await (getPassword().input(inputReceiver).output(outputReceiver).promise());
+			it('should resolve properly', async () => {
+				const then = sinon.fake();
 
-			expect(passwordLength).to.equal(8);
-			expect(inputReceiver.calledOnceWithExactly('password')).to.be.true;
-			expect(inputReceiver.calledAfter(outputReceiver)).to.be.true;
-			expect(inputReceiver.calledBefore(outputReceiver)).to.be.true;
-			expect(outputReceiver.callCount).to.equal(2);
-			expect(outputReceiver.calledWithExactly('asking for password')).to.be.true;
-			expect(outputReceiver.calledWithExactly('got password')).to.be.true;
+				await (buildArray().promise().then(then));
+
+				expect(then).calledOnceWithExactly([0, 1, 2]);
+			});
+
+			it('should properly report output', async () => {
+				const onOutput = sinon.fake();
+
+				await (buildArray().output(onOutput).promise());
+
+				expect(onOutput.callCount).to.equal(3);
+				expect(onOutput).calledWith({msg: 'Pushing data', value: 0});
+				expect(onOutput).calledWith({msg: 'Pushing data', value: 1});
+				expect(onOutput).calledWith({msg: 'Pushing data', value: 2});
+			});
 		});
+
+		describe('with .input()', () => {
+			const postInputSpy = sinon.fake();
+			const getPassword = () => makeAdapter<number, string, {password: string}>(async (resolve, reject, output, input) => {
+				output('asking for password');
+				const password = await input('password');
+				postInputSpy();
+				output('got password');
+				const passwordLength = password.length;
+				resolve(passwordLength);
+			});
+
+			it('should throw an error if no .input() is used', () => {
+				getPassword().promise().catch(error => {
+					expect(postInputSpy.callCount).to.equal(0);
+					expect(error.message).to.equal('Input "password" must be supplied');
+				});
+			});
+
+			it('should receive the input properly', async () => {
+				const inputReceiver = sinon.fake.resolves('pa$$word');
+				const outputReceiver = sinon.fake();
+
+				const passwordLength = await (getPassword().input(inputReceiver).output(outputReceiver).promise());
+
+				expect(passwordLength).to.equal(8);
+				expect(inputReceiver).calledOnceWithExactly('password');
+				expect(inputReceiver).calledAfter(outputReceiver);
+				expect(inputReceiver).calledBefore(outputReceiver);
+				expect(outputReceiver.callCount).to.equal(2);
+				expect(outputReceiver).calledWithExactly('asking for password');
+				expect(outputReceiver).calledWithExactly('got password');
+			});
+		});
+
+		describe('with .then()', () => {
+			const simpleResolver = () => makeAdapter(resolve => resolve(3));
+
+			it('should work using .then() before .promise()', async () => {
+				const then = sinon.spy(x => x);
+
+				let result = await simpleResolver().then(then).promise();
+
+				expect(result).to.equal(3);
+				expect(then).called;
+			});
+		});
+
+		describe('with .catch()', () => {
+			const thrower = () => makeAdapter((resolve, reject) => reject('ERROR'));
+
+			it('should work using .catch() before .promise()', async () => {
+				const cach = sinon.fake();
+
+				await thrower().catch(cach).promise();
+				expect(cach).calledOnceWithExactly('ERROR');
+			});
+		});
+
+		describe('using .attach()', () => {
+			const funky = (succeed: boolean) => makeAdapter<string, string>(async (resolve, reject, output, input) => {
+				if (!succeed) return reject();
+
+				let val = await input('value');
+				output(val);
+
+				resolve(val);
+			});
+
+			it('should work with all attachments at once', async () => {
+				const then = sinon.spy(x => x);
+				const output = sinon.fake();
+				const input = sinon.fake.resolves('blah');
+
+				const result = await funky(true).attach({then, output, input}).exec();
+
+				expect(result).to.equal('blah');
+				expect(then).calledOnceWithExactly('blah');
+				expect(input).calledOnceWithExactly('value');
+				expect(output).calledOnceWithExactly('blah');
+			});
+		});
+	});
+
+	describe('special cases', () => {
+		// TODO
 	});
 });
